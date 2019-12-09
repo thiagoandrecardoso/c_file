e3e843cbb71c1145e101b6e5b29ff90b305d7559
Thiago André Cardoso Silva <thiagoandrecardoso@icloud.com>
2019-12-09 10:48:20 -0300

    file struct
    Programa realiza: Escrita de uma struct em arquivo.txt
    Ler os dados do arquivo e atribui a um struct
    Limpa o arquivo.txt

--- ASCII_file.c
+++ ASCII_file.c
@@ -13,17 +13,17 @@
 /**
 * Declaração das funções
 */
-int lerTexto();
+int escreverTexto();
 int converteArrayCharParaArrayIntASCII_writeInFile(char *texto);
 
 FILE *file;
 
 int main(int argc, char const *argv[]) {
-  lerTexto();
+  escreverTexto();
   return 0;
 }
 
-int lerTexto(){
+int escreverTexto(){
   char *texto;
   texto = (char*) malloc(sizeof(char*) * LEN_TEXT);
   if(!texto) return 0;
@@ -33,17 +33,21 @@ int lerTexto(){
   printf("OK %d", ok);
   return 0;
 }
-
 /**
 * 'w' = abre o arquivo somente para escrita no início do arquivo. Apagará o
 * conteúdo do arquivo se ele já existir, criará um arquivo novo se não
 * existir. (O w vem do inglês write, escrever)
 *
+* a+ = abre o arquivo para escrita no final do arquivo e leitura.
+*
 * strlen retorna a quantidade de caracteres de uma string
 * Em strlen(texto) - 1, o '-1' retira o 'enter' que é armazenado.
 */
 int converteArrayCharParaArrayIntASCII_writeInFile(char *texto){
-  file = fopen("ASCII_int.txt", "w");
+  file = fopen("ASCII_int.txt", "a+");
+  if(file == NULL) {
+    file = fopen("ASCII_int.txt", "w");
+  }
   if(file == NULL) return 0; // retorna 0 se não for possível abrir o arquivo
   for (size_t i = 0; i < strlen(texto) - 1; i++) {
     if(texto[i] <= 0) return 0;
@@ -52,3 +56,5 @@ int converteArrayCharParaArrayIntASCII_writeInFile(char *texto){
   fclose(file); // fecha o arquivo
   return 1;
 }
+// rewind(file); volta para o inicio do arquivo.
+// !feof enquanto nao chegar no final do arquivo.
---
+++ file_struct_read_write.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+/**
+** Thiago André Cardoso Silva
+** Aulas particulares de programação (para iniciantes)
+** Instagram: @programador_who
+** Cel: (085) 999273805
+*/
+
+typedef struct{
+  char nome[20];
+  char endereco[30];
+  char cpf[12];
+  long int telefone;
+  int idade;
+}Aluno;
+
+/**
+* variáveis globais
+*/
+FILE *file;
+char *path = "alunos.txt";
+char *read_type = "w+";
+
+/**
+* declaração das funções.
+*/
+void abrir_arquivp();
+void inserir_dados_no_arquivo();
+void ler_dados_do_arquivo();
+void limpar_arquivo();
+
+int main(int argc, char const *argv[]) {
+  abrir_arquivp();
+  inserir_dados_no_arquivo();
+  ler_dados_do_arquivo();
+  limpar_arquivo();
+  fclose(file);
+  return 0;
+}
+
+void abrir_arquivp(){
+  file = fopen(path, read_type);
+  if(file == NULL) {
+    printf("Erro ao abrir o arquivo");
+    return;
+  }
+}
+
+void inserir_dados_no_arquivo(){
+  Aluno aluno;
+  setbuf(stdin, NULL);
+  printf("Digite o nome: ");
+  fgets(aluno.nome, 20, stdin);
+  printf("Digite o endereço: ");
+  fgets(aluno.endereco, 30, stdin);
+  printf("Digite o CPF: ");
+  fgets(aluno.cpf, 12, stdin);
+  printf("Digite a idade: ");
+  scanf("%d", &aluno.idade);
+  printf("Digite o telefone: ");
+  scanf("%lu", &aluno.telefone);
+  fprintf(file, "%s%s%s%d%lu", aluno.nome, aluno.endereco, aluno.cpf, aluno.idade, aluno.telefone);
+  // if (fwrite(&aluno, sizeof(Aluno), 1, file) != 1) {
+  //   printf("erro ao escrever\n");
+  // }
+}
+
+void ler_dados_do_arquivo(){
+  Aluno aluno;
+  fscanf(file, "%s%s%s%d%lu", aluno.nome, aluno.endereco, aluno.cpf, &aluno.idade, &aluno.telefone);
+  printf("Nome: %s", aluno.nome);
+  printf("Endereço: %s", aluno.endereco);
+  printf("CPF: %s\n", aluno.cpf);
+  printf("Idade: %d\n", aluno.idade);
+  printf("Telefone: %lu\n", aluno.telefone);
+}
+
+void limpar_arquivo(){
+  fclose(fopen(path, read_type));
+}
