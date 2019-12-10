4868f365cdf5d37941d37c9842ada90309b205ea
Thiago André Cardoso Silva <thiagoandrecardoso@icloud.com>
2019-12-09 21:38:22 -0300

    Ajustes CPF
    Alterado espaço do char cpf de 12 para 13

--- file_struct_read_write.c
+++ file_struct_read_write.c
@@ -11,7 +11,7 @@
 typedef struct{
   char nome[20];
   char endereco[30];
-  char cpf[12];
+  char cpf[13];
   long int telefone;
   int idade;
 }Aluno;
@@ -35,7 +35,7 @@ int main(int argc, char const *argv[]) {
   abrir_arquivp();
   inserir_dados_no_arquivo();
   ler_dados_do_arquivo();
-  limpar_arquivo();
+  // limpar_arquivo();
   fclose(file);
   return 0;
 }
@@ -53,18 +53,17 @@ void abrir_arquivp(){
 
 void inserir_dados_no_arquivo(){
   Aluno aluno;
-  setbuf(stdin, NULL);
   printf("Digite o nome: ");
   fgets(aluno.nome, 20, stdin);
   printf("Digite o endereço: ");
   fgets(aluno.endereco, 30, stdin);
   printf("Digite o CPF: ");
-  fgets(aluno.cpf, 12, stdin);
+  fgets(aluno.cpf, 13, stdin);
   printf("Digite a idade: ");
   scanf("%d", &aluno.idade);
   printf("Digite o telefone: ");
   scanf("%lu", &aluno.telefone);
-  fprintf(file, "%s%s%s%d%lu", aluno.nome, aluno.endereco, aluno.cpf, aluno.idade, aluno.telefone);
+  fprintf(file, "%s%s%s%d\n%lu", aluno.nome, aluno.endereco, aluno.cpf, aluno.idade, aluno.telefone);
   // if (fwrite(&aluno, sizeof(Aluno), 1, file) != 1) {
   //   printf("erro ao escrever\n");
   // }
@@ -75,7 +74,7 @@ void ler_dados_do_arquivo(){
   fscanf(file, "%s%s%s%d%lu", aluno.nome, aluno.endereco, aluno.cpf, &aluno.idade, &aluno.telefone);
   printf("Nome: %s", aluno.nome);
   printf("Endereço: %s", aluno.endereco);
-  printf("CPF: %s\n", aluno.cpf);
+  printf("CPF: %s", aluno.cpf);
   printf("Idade: %d\n", aluno.idade);
   printf("Telefone: %lu\n", aluno.telefone);
 }
